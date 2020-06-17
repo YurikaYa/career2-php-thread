72024a8f9b9add89e0e1cc558d7680e5d49e4166
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-06-17 11:04:17 +0900

    削除機能の追加

--- index.php
+++ index.php
@@ -1,17 +1,32 @@
 <html>
-<head><title>掲示板</title></head>
+
+<head><title>掲示板App</title></head>
+
 <body>
 
-<h1>掲示板App</h1>
+
+
+<h1>掲示板</h1>
+
+
 
 <h2>投稿フォーム</h2>
 
+
+
 <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
+
     <input type="text" name="personal_name" placeholder="名前" required><br><br>
+
     <textarea name="contents" rows="8" cols="40" placeholder="内容" required>
+
 </textarea><br><br>
+
     <input type="submit" name="btn" value="投稿する">
+
 </form>
+
+
 
 <h2>スレッド</h2>
 
@@ -19,7 +34,7 @@
 
 <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
 
-    <inpud type="hidden" name="method" value="DELETE">
+    <input type="hidden" name="method" value="DELETE">
 
     <button type="submit">投稿を全削除する</button>
 
@@ -35,70 +50,132 @@ date_default_timezone_set('Asia/Tokyo');
 
 const THREAD_FILE = 'thread.txt';
 
+
+
 function readData() {
+
     // ファイルが存在しなければデフォルト空文字のファイルを作成する
+
     if (! file_exists(THREAD_FILE)) {
+
         $fp = fopen(THREAD_FILE, 'w');
+
         fwrite($fp, '');
+
         fclose($fp);
+
     }
 
-    $thread_text = file_get_contents(THREAD_FILE);
-    echo $thread_text;#5446
 
+
+    $thread_text = file_get_contents(THREAD_FILE);
+
+    echo $thread_text;
 
 }
 
+
+
 function writeData() {
+
     $personal_name = $_POST['personal_name'];
+
     $contents = $_POST['contents'];
+
     $contents = nl2br($contents);
 
+
+
     $data = "<hr>\n";
-    $data = $data."<p>投稿時間:".date("Y-m-d-w a-H:i:s")."</p>\n";
+
+    $data = $data."<p>投稿日時: ".date("Y/m/d H:i:s")."</p>\n";
+
     $data = $data."<p>投稿者:".$personal_name."</p>\n";
+
     $data = $data."<p>内容:</p>\n";
+
     $data = $data."<p>".$contents."</p>\n";
 
-    
+
+
     $fp = fopen(THREAD_FILE, 'a');
 
+    
+
+
+
     if ($fp){
+
         if (flock($fp, LOCK_EX)){
+
             if (fwrite($fp,  $data) === FALSE){
+
                 print('ファイル書き込みに失敗しました');
+
             }
 
+
+
             flock($fp, LOCK_UN);
+
         }else{
+
             print('ファイルロックに失敗しました');
+
         }
+
     }
+
+
 
     fclose($fp);
 
-    // ブラウザのリロード対策//5455
-    $redirect_url = $_SERVER['HTTP_REFERER'];
-    header("Location: $redirect_url");
-    exit;
 }
-function deleteData(){
-    file_put_contents(ThREAD_FILE,"");
+
+
+
+function deleteData() {
+
+    file_put_contents(THREAD_FILE, "");
+
 }
+
+
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    if()isset($_POST["method"]) && $_POST["method"] === "DELETE"{
 
-    }else{
+    if (isset($_POST["method"]) && $_POST["method"] === "DELETE") {
 
+        deleteData();
+
+    } else {
+
+        writeData();
 
     }
-    writeData();
+
+    
+
+    // ブラウザのリロード対策
+
+    $redirect_url = $_SERVER['HTTP_REFERER'];
+
+    header("Location: $redirect_url");
+
+    exit;
+
 }
+
+
 
 readData();
 
+
+
 ?>
 
+
+
 </body>
+
 </html>
